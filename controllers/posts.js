7f09c05957db564e68617f5c0598facc2136c12c
nitn-lab <nitinkudeisa0411@gmail.com>
2023-08-06 01:45:29 +0530

    first commit

---
+++ controllers/posts.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  // res.send("This Works");
+  try {
+    const postMessages = await PostMessage.find();
+    console.log("getPosts controller", postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  //   res.send("Post Created");
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  console.log("createPost controller", newPost);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post with that id");
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: "Post delete successfully" });
+};
