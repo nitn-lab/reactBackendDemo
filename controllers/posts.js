d2c3f9818129c85835f575e2c2cedbd55ad395db
nitn-lab <nitinkudeisa0411@gmail.com>
2023-09-11 18:46:21 +0530

    auth added

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,7 @@ export const getPosts = async (req, res) => {
   // res.send("This Works");
   try {
     const postMessages = await PostMessage.find();
-    console.log("getPosts controller", postMessages);
+    // console.log("getPosts controller", postMessages);
     res.status(200).json(postMessages);
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -15,7 +15,7 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   //   res.send("Post Created");
   const post = req.body;
-  const newPost = new PostMessage(post);
+  const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
   console.log("createPost controller", newPost);
   try {
     await newPost.save();
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-
+import userRouter from "./routes/user.js";
 const app = express();
 dotenv.config();
 
@@ -14,10 +14,11 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-// const CONNECTION_URL = "mongodb+srv://fullStack:fullStack8412@cluster0.fgiv43w.mongodb.net/?retryWrites=true&w=majority";
+app.use("/user", userRouter);
+const CONNECTION_URL = "mongodb+srv://fullStack:fullStack8412@cluster0.fgiv43w.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
 .catch((error) => console.log(error.message))
 
