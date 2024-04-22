650bef4b4849503a8489fc246a42d154e58f7786
Nitin Kudesia <54732720+nitn-lab@users.noreply.github.com>
2024-04-22 15:42:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express();
 dotenv.config();
 
 
-app.use(bodyParser.json({ limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
+// app.use(bodyParser.json({ limit: '30mb', extended: true}));
+// app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
